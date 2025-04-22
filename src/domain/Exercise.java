b48b872a70849ede1b48fb98e47f7c383bac2ba0
StasIhnatishin <137786669+StasIhnatishin@users.noreply.github.com>
2025-04-22 14:18:19 +0300

    exercies

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,17 @@
 package domain;
 
-public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+public class Exercies {
+
+    public static boolean Calculate(int number) {
+        int original = number;
+        int digits = (int) Math.log10(number) + 1;
+        int sum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += Math.pow(digit, digits);
+            number /= 10;
+        }
+        return sum == original;
     }
 }
